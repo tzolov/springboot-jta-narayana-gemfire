655e5c1a8eefb7d0cd309302306e1f04967ac630
Christian Tzolov <christian.tzolov@gmail.com>
2016-10-16 18:01:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-jta-narayana-gemfire
---
+++ src/main/java/sample/narayana/Account.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample.narayana;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	private String username;
+
+	public Account() {
+	}
+
+	public Account(Long id, String username) {
+		this.id = id;
+		this.username = username;
+	}
+
+	public Account(String username) {
+		this.username = username;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
